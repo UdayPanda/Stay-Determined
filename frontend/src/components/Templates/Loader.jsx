6a0c10c6ebe158addffe5213c7a7859eb93b17ea
Uday Panda <97422449+UdayPanda@users.noreply.github.com>
2024-12-10 23:57:27 +0530

    Create Loader.jsx
    Created a loader component.

---
+++ frontend/src/components/Templates/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Loader() {
+  return (
+    <>
+    <div className='absolute top-[35%] left[35%] h-[100vh] text-2xl font-bold color-white'>Loading....</div>
+    </>
+    
+  )
+
+}
+
+export default Loader
