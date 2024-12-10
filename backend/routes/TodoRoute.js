c6525f90845c995e0796f35e39ff3878837b91d9
Uday Panda <97422449+UdayPanda@users.noreply.github.com>
2024-12-11 00:01:09 +0530

    Update TodoRoute.js
    Added a route as count.

--- backend/routes/TodoRoute.js
+++ backend/routes/TodoRoute.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
-import { addTodo, getTodos, deleteTodo, updateTodo } from "../controllers/TodoController.js";
+import { addTodo, getTodos, deleteTodo, updateTodo, getExpanseTodos, getLabelCounts } from "../controllers/TodoController.js";
 
 
 const todoRoute = Router()
 
 todoRoute.post('/add', addTodo)
 todoRoute.post('/get', getTodos)
+todoRoute.post('/count', getLabelCounts)
 todoRoute.delete('/delete/:id', deleteTodo)
 todoRoute.post('/update', updateTodo)
+todoRoute.post('/loan', getExpanseTodos)
 
 export default todoRoute
